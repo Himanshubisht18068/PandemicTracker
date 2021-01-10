043a39caaeb18ccd3352aca52f8a112d53ad4e2b
Himanshu Bisht <himanshubisht18068@gmail.com>
2021-01-10 18:37:39 +0530

    create required Methods skeleton

--- src/main/java/bisht/b/PandemicTracker/PandemicManager/PandemicManager.java
+++ src/main/java/bisht/b/PandemicTracker/PandemicManager/PandemicManager.java
@@ -1,4 +1,42 @@
 package bisht.b.PandemicTracker.PandemicManager;
 
 public class PandemicManager {
+
+    public void report(String diseaseName, String patientID, String countryName, String stateName, String cityName){
+
+    }
+
+    public void cured(String diseaseName, String patientID){
+
+    }
+
+    public void fatal(String diseaseName, String patientID){
+
+    }
+
+    public String showWorldSummary(){
+
+        return null;
+
+    }
+
+    public String showWorldSummaryDiseasesBreakup(){
+
+        return null;
+
+    }
+
+    public String showCountryBreakup(String diseaseName){
+
+        return null;
+
+    }
+
+    public String ShowStateBreakup(String diseaseName, String countryName){
+
+        return null;
+
+    }
+
+
 }
