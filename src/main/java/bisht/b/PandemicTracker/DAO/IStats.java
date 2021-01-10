25155e0ebb97cfddb54f53c4568d5ad68a49a376
Himanshu Bisht <himanshubisht18068@gmail.com>
2021-01-10 21:03:34 +0530

    Stats.java created members

--- src/main/java/bisht/b/PandemicTracker/DAO/IStats.java
+++ src/main/java/bisht/b/PandemicTracker/DAO/IStats.java
@@ -1,4 +1,7 @@
 package bisht.b.PandemicTracker.DAO;
 
 public interface IStats {
+
+    public String getStats(String name);
+
 }
--- src/main/java/bisht/b/PandemicTracker/DAO/Stats.java
+++ src/main/java/bisht/b/PandemicTracker/DAO/Stats.java
@@ -1,4 +1,15 @@
 package bisht.b.PandemicTracker.DAO;
 
 public class Stats implements IStats{
+
+    private int totalCases;
+    private int curedCases;
+    private int fatalCases;
+    private int activeCases;
+
+
+    @Override
+    public String getStats(String name) {
+        return null;
+    }
 }
