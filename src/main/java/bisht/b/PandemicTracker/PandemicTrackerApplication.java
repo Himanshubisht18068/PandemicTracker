b2b94250f472cfde14f6f6bdc1e6178bcf2a34ef
Himanshu_Bisht <Himanshubisht18068@gmail.com>
2021-01-10 18:08:36 +0530

    intial commit

---
+++ src/main/java/bisht/b/PandemicTracker/PandemicTrackerApplication.java
@@ -0,0 +1,13 @@
+package bisht.b.PandemicTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandemicTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandemicTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bisht/b/PandemicTracker/PandemicTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package bisht.b.PandemicTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
